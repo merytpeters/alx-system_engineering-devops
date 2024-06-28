b00fdbb0a6220377edec94772161e7d8a851227b
merytpeters <merytpeters@gmail.com>
2024-06-28 10:48:22 -0700

    Created a directory tmp and file school using puppet, added package install for werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,11 @@
 
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3'
+}
+
+# Install Werkzeung version 2.1.1
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
