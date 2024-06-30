552adc59f7fd9321bcbae4469554731468ea0712
merytpeters <merytpeters@gmail.com>
2024-06-30 16:08:16 -0700

    Trying to get the correct output, path changed to /etc..

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 # Make changes, set up client SSH config file
 
-file { '/root/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure  => 'present',
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
   content => "\
 Host *
-    HostName 3.84.238.65
     PasswordAuthentication no
     IdentityFile ~/.ssh/school
 ",
