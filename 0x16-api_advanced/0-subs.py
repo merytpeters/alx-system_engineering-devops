c93081da2a3524625de927661cf612e2b28f40a4
merytpeters <merytpeters@gmail.com>
2024-08-13 13:23:45 +0100

    Queries reddit api and return number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Queries the Reddit API and returns the number of subscribers"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Gets number of subscribers"""
+
+    base_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom-script/1.0'}
+
+    try:
+        response = requests.get(base_url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
