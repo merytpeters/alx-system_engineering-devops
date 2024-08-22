3fa6ccb69a6eadc45058eb2111a2103d3853765b
merytpeters <merytpeters@gmail.com>
2024-08-22 22:43:33 +0000

    Increase soft and hard limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -10,14 +10,14 @@ user { 'holberton':
 
 # Ensure the soft limit for open files is set for the holberton user
 exec { 'set_soft_nofile_limit':
-  command => 'echo "holberton soft nofile 65536" >> /etc/security/limits.conf',
-  unless  => 'grep -q "^holberton soft nofile" /etc/security/limits.conf',
-  path    => ['/bin', '/usr/bin'],
+  command => '/bin/sed -i "/^holberton.*soft.*nofile/c\holberton soft nofile 50000" /etc/security/limits.conf',
+  unless  => '/bin/grep -q "^holberton soft nofile 50000" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', 'bin', '/usr/bin'],
 }
 
 # Ensure the hard limit for open files is set for the holberton user
 exec { 'set_hard_nofile_limit':
-  command => 'echo "holberton hard nofile 65536" >> /etc/security/limits.conf',
-  unless  => 'grep -q "^holberton hard nofile" /etc/security/limits.conf',
-  path    => ['/bin', '/usr/bin'],
+  command => '/bin/sed -i "/^holberton.*hard.*nofile/c\holberton hard nofile 50000" /etc/security/limits.conf',
+  unless  => '/bin/grep -q "^holberton hard nofile 50000" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', 'bin', '/usr/bin'],
 }
