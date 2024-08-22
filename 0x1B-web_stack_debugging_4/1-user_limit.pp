cca5b00e1af5dc13a2914b390e195a0d97dea19b
merytpeters <merytpeters@gmail.com>
2024-08-22 21:59:37 +0000

    Hard and soft limit increased

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,4 +3,21 @@
 user { 'holberton':
   ensure     => 'present',
   managehome => true,
+  shell      => '/bin/bash',
+  home       => '/home/holberton',
+  groups     => ['sudo', 'www-data'],
+}
+
+# Ensure the soft limit for open files is set for the holberton user
+exec { 'set_soft_nofile_limit':
+  command => 'echo "holberton soft nofile 65536" >> /etc/security/limits.conf',
+  unless  => 'grep -q "^holberton soft nofile" /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin'],
+}
+
+# Ensure the hard limit for open files is set for the holberton user
+exec { 'set_hard_nofile_limit':
+  command => 'echo "holberton hard nofile 65536" >> /etc/security/limits.conf',
+  unless  => 'grep -q "^holberton hard nofile" /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin'],
 }
